89397cab19cb01368ea5835c7bda352dd11538f0
Alex <alexmath.it@mail.ru>
2024-11-20 15:08:57 +0300

    implement lab-4

---
+++ script.js
@@ -0,0 +1,41 @@
+
+window.addEventListener('load', () => {
+    const loadTime = performance.timing.domContentLoadedEventEnd - performance.timing.navigationStart;
+    const footer = document.querySelector('footer');
+    const loadInfo = document.createElement('p');
+    loadInfo.textContent = `Страница загрузилась за ${loadTime} миллисекунд.`;
+    footer.appendChild(loadInfo);
+});
+
+const navLinks = document.querySelectorAll('.nav-links a');
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = 'gold';
+        link.style.backgroundColor = '#444';
+        link.style.padding = '5px';
+        link.style.borderRadius = '5px';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.color = '';
+        link.style.backgroundColor = '';
+        link.style.padding = '';
+        link.style.borderRadius = '';
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const currentHash = document.location.hash; // Получаем текущий якорь (например, #shop)
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentHash) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
+
+
